8b919a7b0af212b94b79faad6a016be5eccbd5dc
Mike Lines <lines0812@gmail.com>
2026-07-22 14:07:03 +1200

    feat: output event activities

--- main.py
+++ main.py
@@ -3,6 +3,24 @@ from urllib.error import URLError
 from urllib.request import Request, urlopen
 
 
+def format_event_output(
+    event_type: str, repo_name: str, event_count: int
+) -> str | None:
+    match event_type:
+        case "CreateEvent":
+            output = f"- Created {str(event_count) + ' branch' if event_count == 1 else str(event_count) + ' branches'} in {repo_name}"
+        case "PushEvent":
+            output = f"- Pushed {str(event_count)} commit(s) to {repo_name}"
+        case "WatchEvent":
+            output = f"- Starred {repo_name}"
+        case "PullRequestReviewEvent":
+            output = f"- Reviewed {str(event_count)} pull request(s) for {repo_name}"
+        case _:
+            return None
+
+    return output
+
+
 def get_github_activity(username: str) -> list[dict] | None:
     # test if username exists
     req = Request(f"https://api.github.com/users/{username}/events")
@@ -20,7 +38,6 @@ def get_github_activity(username: str) -> list[dict] | None:
     else:
         user_activity = response.read().decode("utf8")
         events = json.loads(user_activity)
-        print(events)
         return events
 
 
@@ -37,9 +54,17 @@ def group_events(gh_events: list[dict]):
             event_grps[event_type][repo_name] = 0
 
         event_grps[event_type][repo_name] += 1
-
-    print(event_grps)
     return event_grps
+
+
+def output_event_groups(event_grps: list):
+    print("Output:")
+
+    for event_type, repos in event_grps.items():
+        for repo, count in repos.items():
+            line = format_event_output(event_type, repo, count)
+            if line:
+                print(line)
 
 
 def main():
@@ -47,6 +72,7 @@ def main():
     if events:
         # roll up events
         grouped_events = group_events(events)
+        output_event_groups(grouped_events)
 
 
 if __name__ == "__main__":
